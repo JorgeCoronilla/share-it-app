82eede6025566c29941e161c26011da7018192b4
jorge <jorge.coronilla.naranjo@gmail.com>
2023-12-11 20:15:53 +0100

    [Ux] Home and switcher adjusments for mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main>
       <div className="blur-stain-1"></div>
       <div className="blur-stain-2"></div>
-      <div className="flex flex-col items-center justify-center pt-80 pb-10 px-10">
+      <div className="flex flex-col items-center justify-center pt-80 pb-10 px-10 home-body">
         <h1 className="title">Share-it</h1>
         <p className="normal-text mt-10">
           Do not forget what you paid, not forget to pay.
