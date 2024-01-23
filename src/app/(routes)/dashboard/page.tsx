fe473c69549ab84cc2c045bf1fc2bd5514f97461
jorge <jorge.coronilla.naranjo@gmail.com>
2024-01-23 19:56:17 +0100

    [Fix] missing imports references

--- src/app/(routes)/dashboard/page.tsx
+++ src/app/(routes)/dashboard/page.tsx
@@ -1,12 +1,5 @@
-import { Suspense } from 'react';
 import GroupsList from '../../ui/dashboard/groups/groupsList';
 import SectionTitle from '@/app/ui/global/sectionTitle';
-import AddGroupButton from './create-group/page';
-import ThemeSwitcher from '@/app/ui/global/themeSwitcher';
-import AddFriendButton from '@/app/ui/dashboard/buttons/addFriend';
-import AddExpenseButton from './create-expense/page';
-import { IconDeleteBin } from '@/app/ui/dashboard/iconsComponents/deleteBin';
-import { useParams, useRouter, useSearchParams } from 'next/navigation';
 
 export default function Page() {
   return (
@@ -15,7 +8,6 @@ export default function Page() {
       <SectionTitle title="Grupos" />
       <GroupsList />
       {/* </Suspense> */}
-      <ThemeSwitcher pageClass="dashboard" />
 
       <div className="spacer"></div>
     </>
