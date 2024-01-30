c5cd9a2af6c9b2c8ea2950b2ee865500321f04be
jorge <jorge.coronilla.naranjo@gmail.com>
2024-01-30 10:54:58 +0100

    [Fix] Missing imports

--- src/app/ui/components/home/homeHeader.tsx
+++ src/app/ui/components/home/homeHeader.tsx
@@ -1,7 +1,6 @@
 'use client';
 import Link from 'next/link';
 import React from 'react';
-import ThemeSwitcher from '../components/global/themeSwitcher';
 function HomeHeader() {
   return (
     <div>
