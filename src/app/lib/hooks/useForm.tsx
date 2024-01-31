f95adab0d3c711e9bf581bd9278ea8da4c0064fa
JorgeCoronilla <jorge.coronilla.naranjo@gmail.com>
2024-01-31 08:55:43 +0100

    [Test] cleaning logs

--- src/app/lib/hooks/useForm.tsx
+++ src/app/lib/hooks/useForm.tsx
@@ -36,35 +36,33 @@ export const useForm = () => {
     setLoading(false);
     if (userLogged.status === 200) {
       router.push(`/register/check-email`);
-      console.log(userLogged);
-    }
-    if (userLogged.status === 400) {
-      setErrorMessage('Complete todos los campos');
-      console.log(userLogged);
-    }
+    } else {
+      if (userLogged.status === 400) {
+        setErrorMessage('Complete todos los campos');
+      }
 
-    if (userLogged.status === 404) {
-      setErrorMessage('Ya existe un usuario con ese email');
-      console.log(userLogged);
-    }
+      if (userLogged.status === 404) {
+        setErrorMessage('Ya existe un usuario con ese email');
+      }
 
-    if (userLogged.status === 404) {
-      setErrorMessage('Ya existe un usuario con ese email. Confirma tu correo');
-      console.log(userLogged);
-    }
+      if (userLogged.status === 404) {
+        setErrorMessage(
+          'Ya existe un usuario con ese email. Confirma tu correo'
+        );
+      }
 
-    if (userLogged.status === 500) {
-      setErrorMessage(
-        'Algo ha ido mal, inténtelo más tarde o contacte con Share-it'
-      );
-      console.log(userLogged);
-    }
-    setTimeout(() => {
-      router.push(`/dashboard`);
-    }, 2000);
-    setError(true);
+      if (userLogged.status === 500) {
+        setErrorMessage(
+          'Algo ha ido mal, inténtelo más tarde o contacte con Share-it'
+        );
+      }
+      setTimeout(() => {
+        router.push(`/dashboard`);
+      }, 2000);
+      setError(true);
 
-    console.log('Error:', userLogged.status);
+      console.log('Error:', userLogged.status);
+    }
   };
   return { getData, submit, showError, onFocus, error, errorMessage, loading };
 };
