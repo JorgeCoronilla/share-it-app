ef8ab70f110351f7777b6acaccabbac8f37041df
JorgeCoronilla <jorge.coronilla.naranjo@gmail.com>
2024-01-31 08:50:01 +0100

    [Test] Adding logs to register flow

--- src/app/lib/hooks/useForm.tsx
+++ src/app/lib/hooks/useForm.tsx
@@ -34,36 +34,37 @@ export const useForm = () => {
       password: checkData.password,
     });
     setLoading(false);
-    if (userLogged.ok) {
+    if (userLogged.status === 200) {
       router.push(`/register/check-email`);
-    } else {
-      if (userLogged.status === 400) {
-        setErrorMessage('Complete todos los campos');
-      }
-
-      if (userLogged.status === 404) {
-        setErrorMessage('Ya existe un usuario con ese email');
-      }
-
-      if (userLogged.status === 404) {
-        setErrorMessage(
-          'Ya existe un usuario con ese email. Confirma tu correo'
-        );
-      }
-
-      if (userLogged.status === 500) {
-        setErrorMessage(
-          'Algo ha ido mal, inténtelo más tarde o contacte con Share-it'
-        );
-      }
-      setTimeout(() => {
-        router.push(`/dashboard`);
-      }, 2000);
-      setError(true);
-
-      setError(true);
-      console.log('Error:', userLogged.status);
+      console.log(userLogged);
     }
+    if (userLogged.status === 400) {
+      setErrorMessage('Complete todos los campos');
+      console.log(userLogged);
+    }
+
+    if (userLogged.status === 404) {
+      setErrorMessage('Ya existe un usuario con ese email');
+      console.log(userLogged);
+    }
+
+    if (userLogged.status === 404) {
+      setErrorMessage('Ya existe un usuario con ese email. Confirma tu correo');
+      console.log(userLogged);
+    }
+
+    if (userLogged.status === 500) {
+      setErrorMessage(
+        'Algo ha ido mal, inténtelo más tarde o contacte con Share-it'
+      );
+      console.log(userLogged);
+    }
+    setTimeout(() => {
+      router.push(`/dashboard`);
+    }, 2000);
+    setError(true);
+
+    console.log('Error:', userLogged.status);
   };
   return { getData, submit, showError, onFocus, error, errorMessage, loading };
 };
