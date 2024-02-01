0d9473865489d95c61b72ca38bcfda0e61f7cf95
JorgeCoronilla <jorge.coronilla.naranjo@gmail.com>
2024-02-01 14:08:56 +0100

    [Add] Loading skeleton

--- src/app/(app-pages)/dashboard/loading.tsx
+++ src/app/(app-pages)/dashboard/loading.tsx
@@ -1,83 +1,106 @@
+import Header from '@/app/ui/components/dashboard/header/header';
+import Menu from '@/app/ui/components/dashboard/menu/menu';
 import React from 'react';
 import ContentLoader from 'react-content-loader';
 
 export default function Loading() {
   return (
-    <div className="emptyDash">
-      <p>Loading</p>
-    </div>
-    // <ContentLoader
-    //   speed={2}
-    //   width={400}
-    //   height={160}
-    //   viewBox="0 0 400 160"
-    //   backgroundColor="#f3f3f3"
-    //   foregroundColor="#ecebeb"
-    // >
-    //   <rect
-    //     x="-4"
-    //     y="172"
-    //     rx="3"
-    //     ry="3"
-    //     width="88"
-    //     height="6"
-    //   />
-    //   <rect
-    //     x="170"
-    //     y="14"
-    //     rx="0"
-    //     ry="0"
-    //     width="0"
-    //     height="1"
-    //   />
-    //   <rect
-    //     x="125"
-    //     y="89"
-    //     rx="0"
-    //     ry="0"
-    //     width="1"
-    //     height="0"
-    //   />
-    //   <rect
-    //     x="216"
-    //     y="155"
-    //     rx="0"
-    //     ry="0"
-    //     width="7"
-    //     height="10"
-    //   />
-    //   <rect
-    //     x="30"
-    //     y="109"
-    //     rx="0"
-    //     ry="0"
-    //     width="287"
-    //     height="80"
-    //   />
-    //   <rect
-    //     x="242"
-    //     y="62"
-    //     rx="0"
-    //     ry="0"
-    //     width="0"
-    //     height="58"
-    //   />
-    //   <rect
-    //     x="32"
-    //     y="57"
-    //     rx="0"
-    //     ry="0"
-    //     width="179"
-    //     height="11"
-    //   />
-    //   <rect
-    //     x="30"
-    //     y="6"
-    //     rx="0"
-    //     ry="0"
-    //     width="144"
-    //     height="27"
-    //   />
-    // </ContentLoader>
+    <>
+      <Header />
+      <Menu />
+      <ContentLoader
+        speed={2}
+        width={280}
+        height={400}
+        viewBox="0 0 280 400"
+        backgroundColor="#c96bff0c"
+        foregroundColor="#f881ca44"
+      >
+        <rect
+          x="40"
+          y="115"
+          rx="6"
+          ry="6"
+          width="140"
+          height="30"
+        />
+
+        <rect
+          x="40"
+          y="170"
+          rx="4"
+          ry="4"
+          width="200"
+          height="15"
+        />
+
+        <rect
+          x="40"
+          y="230"
+          rx="4"
+          ry="4"
+          width="240"
+          height="3"
+        />
+        <rect
+          x="40"
+          y="315"
+          rx="4"
+          ry="4"
+          width="240"
+          height="3"
+        />
+
+        <rect
+          x="40"
+          y="230"
+          rx="4"
+          ry="4"
+          width="3"
+          height="87"
+        />
+        <rect
+          x="277"
+          y="230"
+          rx="4"
+          ry="4"
+          width="3"
+          height="87"
+        />
+        <rect
+          x="55"
+          y="245"
+          rx="4"
+          ry="4"
+          width="130"
+          height="15"
+        />
+        <rect
+          x="55"
+          y="270"
+          rx="4"
+          ry="4"
+          width="100"
+          height="12"
+        />
+        <rect
+          x="55"
+          y="292"
+          rx="4"
+          ry="4"
+          width="110"
+          height="12"
+        />
+
+        <rect
+          x="230"
+          y="260"
+          rx="4"
+          ry="4"
+          width="40"
+          height="46"
+        />
+      </ContentLoader>
+    </>
   );
 }
