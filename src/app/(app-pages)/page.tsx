c187e4ff637763a79be98fd921cd56556a1ddf6f
JorgeCoronilla <jorge.coronilla.naranjo@gmail.com>
2024-03-04 10:18:47 +0100

    [A11y] Changing roles of decoration elements

--- src/app/(app-pages)/page.tsx
+++ src/app/(app-pages)/page.tsx
@@ -7,8 +7,14 @@ export default function Home() {
       <section className="home-body">
         <HomeHeader />
         <ThemeSwitcher pageClass="home" />
-        <div className="blur-stain-1"></div>
-        <div className="blur-stain-2"></div>
+        <div
+          role="presentation"
+          className="blur-stain-1"
+        ></div>
+        <div
+          role="presentation"
+          className="blur-stain-2"
+        ></div>
         <main>
           <div className="hero-container">
             <h1 className="title">Share-it</h1>
